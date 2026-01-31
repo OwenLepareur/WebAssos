c8b775764f4e3c0ab24f4e7c80972c5e064dbcef
Ethan Lepareur <166405645+Tatanelprr@users.noreply.github.com>
2026-01-31 17:41:34 +0100

    Refactor login page layout and styles for improved user experience

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-if (isset($_SESSION["admin"])) {
+// Si déjà connecté, on renvoie direct vers l'admin
+if (isset($_SESSION["admin"]) && $_SESSION["admin"] === true) {
     header("Location: admin.php");
     exit;
 }
@@ -10,22 +11,37 @@ $error = isset($_GET["error"]);
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion Admin</title>
     <link rel="stylesheet" href="assets/css/Login.css">
 </head>
 <body>
-    <div id = "LoginFrame">
-        <h2>Connexion Admin</h2>
+
+    <div id="LoginContainer">
+        <div class="logo-container">
+            <img src="assets/img/ui/GNLogo.jpg" alt="Logo GN">
+        </div>
+
+        <h2>Espace Admin</h2>
 
         <?php if ($error): ?>
-            <p>Identifiants incorrects</p>
+            <div class="error-msg">Identifiants incorrects !</div>
         <?php endif; ?>
 
-        <form method="POST" action="login.php">
-            <input type="text" name="username" placeholder="Nom d'utilisateur" required>
-            <input type="password" name="password" placeholder="Mot de passe" required>
-            <button>Se connecter</button>
+        <form method="POST" action="../app/controllers/auth.php"> 
+        <div class="input-group">
+                <input type="text" name="username" placeholder="Nom d'utilisateur" required>
+            </div>
+
+            <div class="input-group">
+                <input type="password" name="password" placeholder="Mot de passe" required>
+            </div>
+
+            <button type="submit">Se connecter</button>
         </form>
+
+        <a href="index.html" class="back-link">← Retour au site</a>
     </div>
+
 </body>
 </html>
